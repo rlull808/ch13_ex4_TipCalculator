56498e6da6dd186a6b99e2dc289101d0fec3a988
Robert Lull <r_lull@hotmail.com>
2015-09-24 22:12:00 -0700

    Project 6 Changes

--- app/src/main/java/com/murach/tipcalculator/TipCalculatorActivity.java
+++ app/src/main/java/com/murach/tipcalculator/TipCalculatorActivity.java
@@ -1,9 +1,12 @@
 package com.murach.tipcalculator;
 
 import java.text.NumberFormat;
+import java.util.ArrayList;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -15,6 +18,7 @@ import android.widget.TextView.OnEditorActionListener;
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
+import android.widget.Toast;
 
 public class TipCalculatorActivity extends Activity 
 implements OnEditorActionListener, OnClickListener {
@@ -24,15 +28,23 @@ implements OnEditorActionListener, OnClickListener {
     private TextView percentTextView;   
     private Button   percentUpButton;
     private Button   percentDownButton;
+    private Button   buttonSaveTip;
     private TextView tipTextView;
     private TextView totalTextView;
     
     // define instance variables that should be saved
     private String billAmountString = "";
     private float tipPercent = .15f;
+    private final String TAG = "com.murach.tipcalculator";
+    private long lastTipID;
     
     // set up preferences
     private SharedPreferences prefs;
+
+    // set up a tips database
+    private TipsDB tipsDB;
+
+
     
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -44,6 +56,7 @@ implements OnEditorActionListener, OnClickListener {
         percentTextView = (TextView) findViewById(R.id.percentTextView);
         percentUpButton = (Button) findViewById(R.id.percentUpButton);
         percentDownButton = (Button) findViewById(R.id.percentDownButton);
+        buttonSaveTip = (Button) findViewById(R.id.buttonSaveTip);
         tipTextView = (TextView) findViewById(R.id.tipTextView);
         totalTextView = (TextView) findViewById(R.id.totalTextView);
 
@@ -51,6 +64,9 @@ implements OnEditorActionListener, OnClickListener {
         billAmountEditText.setOnEditorActionListener(this);
         percentUpButton.setOnClickListener(this);
         percentDownButton.setOnClickListener(this);
+        buttonSaveTip.setOnClickListener(this);
+
+
         
         // get default SharedPreferences object
         prefs = PreferenceManager.getDefaultSharedPreferences(this);        
@@ -67,6 +83,7 @@ implements OnEditorActionListener, OnClickListener {
         super.onPause();      
     }
     
+    @SuppressLint("LongLogTag")
     @Override
     public void onResume() {
         super.onResume();
@@ -79,7 +96,21 @@ implements OnEditorActionListener, OnClickListener {
         billAmountEditText.setText(billAmountString);
         
         // calculate and display
+
+
+        tipsDB = new TipsDB(this);
+        ArrayList<Tip> tipsList = tipsDB.getTips();
+
+        for (Tip tip : tipsList)
+        {
+            Log.i(TAG, "Tip Data: " + tip.getId() + " " + tip.getDateMillis() + "  $" +
+                    tip.getBillAmount() + " " + tip.getTipPercent());
+        }
+        Tip lastTip = tipsDB.getLastTipDate();
+        Log.i("com.lull.TipCalculator", "Last Tip Date: " + lastTip.getDateStringFormatted());
+        Log.i("com.lull.TipCalculator", "Average Tip Percent: %" + (tipsDB.setAvgTipPercent() * 100));
         calculateAndDisplay();
+
     }
     
     public void calculateAndDisplay() {        
@@ -127,6 +158,34 @@ implements OnEditorActionListener, OnClickListener {
             tipPercent = tipPercent + .01f;
             calculateAndDisplay();
             break;
+            case R.id.buttonSaveTip:
+                Tip newTip = saveTip();
+                if (newTip == null){
+                    break;
+                }
+                else{
+                    tipsDB.saveTip(newTip);
+                    billAmountEditText.setText("");
+                }
+                tipPercent = tipsDB.setAvgTipPercent();
+                calculateAndDisplay();
+                break;
+            default:
         }
+    }
+
+    public Tip saveTip(){
+        Tip newTip;
+        if (billAmountEditText.getText().toString().equals("")){
+            Toast.makeText(this, "Please enter a Bill Amount before Saving Tips", Toast.LENGTH_LONG).show();
+            newTip = null;
+        }
+        else{
+            newTip = new Tip();
+            newTip.setBillAmount(Float.parseFloat(billAmountEditText.getText().toString()));
+            newTip.setTipPercent(tipPercent);
+            newTip.setDateMillis(System.currentTimeMillis());
+        }
+        return newTip;
     }
 }
